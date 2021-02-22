2be53b5c588d6bf4c4564211b1f6626434d2b741
Jair Alayon <jair.atl.95@gmail.com>
2021-02-21 22:43:10 -0600

    Completing the quiz questions activity

--- CountryQuizApp/app/src/main/java/com/alayon/countryquizapp/QuizQuestionsActivity.kt
+++ CountryQuizApp/app/src/main/java/com/alayon/countryquizapp/QuizQuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.alayon.countryquizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_quiz_questions.*
 
 class QuizQuestionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +12,18 @@ class QuizQuestionsActivity : AppCompatActivity() {
 
         val questionList = Constants.getQuestions()
         Log.i("Questions size", "${questionList.size}")
+
+        val currentPosition = 1
+        val question : Question? = questionList[currentPosition-1]
+        progressBar.progress = currentPosition
+
+        txtProgress.text = "$currentPosition / {${progressBar.max}}"
+
+        txtQuestion.text = question!!.question
+        imgCountry.setImageResource(question.image)
+        txtOptionOne.text = question!!.option1
+        txtOptionTwo.text = question!!.option2
+        txtOptionThree.text = question!!.option3
+        txtOptionFour.text = question!!.option4
     }
 }
